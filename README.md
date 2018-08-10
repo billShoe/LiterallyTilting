a2b3e3e15d221e9514c2bcd15f2bbb5bfffe3124
946336 <wesleywei88@gmail.com>
2018-08-10 05:39:55 -0400

    Added readme, gravity, and "hidden" feature toggles

---
+++ README.md
@@ -0,0 +1,30 @@
+Literally Tilting
+=================
+
+A volume selector for perpetual masochists. This is bad volume selection taken
+to an extreme: you don't even get to keep your selection!
+
+Features
+========
+
+### Drift
+
+`FEATURE_DRIFT` (`true` by default)
+
+The balance sliders will drift
+
+`FEATURE_DRIFT_STOP` (`false` by default)
+
+The balance sliders will drift, but only sometimes. Requires `FEATURE_DRIFT`.
+
+`FEATURE_DRIFT_TWIST` (`false` by default)
+
+The balance sliders will occasionally disagree and head in opposite directions.
+Requires `FEATURE_DRIFT`.
+
+### Gravity
+
+`FEATURE_GRAVITY` (`false` by default)
+
+The volume slider will experience a mockery of gravity.
+
--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 var sliderLeft,
 	sliderRight,
 	volumeBar,
-	volumeDisplay,
-	rand;
+	volumeDisplay;
+
+var leftDirection,
+	rightDirection;
+	// Also, oneDirection
+
 var barRotation = 0;
+var maxBarRotation = 45;
+var maxVolumeScale = 100;
+
+var milliseconds = 1000;
+var ticksPerSecond = 100;
+
+var tickSize          = 1;
+var gravityFuckFactor = 0.5;
+var gravityTick       = 10 * tickSize * gravityFuckFactor;
+
+var gravityFlag     = 0;
+
+var DEBUG = false;
+var FEATURE_GRAVITY = false;
+var FEATURE_DRIFT = true;
+var FEATURE_DRIFT_TWIST = false;
+var FEATURE_DRIFT_STOP = false;
 
 window.onload = function () {
 	volumeBar = document.getElementById('volumeBar');
@@ -14,34 +35,97 @@ window.onload = function () {
 	prepareFuckery();
 }
 
+function setVolume() {
+	rotation = ((sliderLeft.value / sliderLeft.max) - (sliderRight.value / sliderRight.max)) * maxBarRotation;
+	rotation = clamp(rotation, -maxBarRotation, maxBarRotation);
+	volumeBar.style.transform = `rotate(${rotation}deg)`;
+
+	volumeBar.value = (parseInt(volumeBar.value)) + Math.round(rotation);
+
+	volumeDisplay.innerText = Math.round(volumeBar.value / maxVolumeScale);
+}
+
+dirs = {
+	"-1": "down",
+	"0": "nowhere",
+	"1": "up"
+};
+function getDriftDirection() {
+	if (!FEATURE_DRIFT_STOP) {
+		// [-1, 1]
+		return Math.round(Math.random()) ? 1 : -1;
+	} else {
+		// [-1, 0, 1]
+		return Math.round(Math.random() * 2) - 1;
+	}
+}
+
+function setBehavior() {
+	if (FEATURE_DRIFT_TWIST) {
+		rightDirection = getDriftDirection();
+		leftDirection  = getDriftDirection();
+
+		if (DEBUG) {
+		console.log(`setBehavior: Left drifting ${dirs[leftDirection]}, ` +
+				`Right drifting ${dirs[rightDirection]}`);
+		}
+	} else {
+		leftDirection = rightDirection = getDriftDirection();
+
+		if (DEBUG) {
+			console.log(`setBehavior: Drifting ${dirs[leftDirection]}`);
+		}
+	}
+}
+
 function prepareFuckery() {
-	function setVolume(angle) {
-		volumeBar.value = (volumeBar.value) - Math.round(angle) * -1;
-		volumeDisplay.innerText = Math.round(volumeBar.value / 100);
-	}
+	setBehavior();
 
 	setInterval(function () {
-		rand = Math.round(Math.random());
-		startFuckery();
-	}, 10000);
+		setBehavior();
+	}, milliseconds * 10);
 
 	setInterval(function () {
-		barRotation = ((sliderLeft.value / sliderLeft.max) - (sliderRight.value / sliderRight.max)) * 45;
-		volumeBar.style.transform = 'rotate(' + barRotation + 'deg)';
-		setVolume(barRotation);
-	}, 10)
+		doFuckery();
+		setVolume();
+	}, Math.round(milliseconds / ticksPerSecond));
 }
 
-function startFuckery() {
-	if (rand === 0) {
-		setInterval(function () {
-			sliderLeft.value = sliderLeft.value - 1;
-			sliderRight.value = sliderRight.value - 1;
-		}, 10)
-	} else if (rand === 1) {
-		setInterval(function () {
-			sliderLeft.value = sliderLeft.value - -1;
-			sliderRight.value = sliderRight.value - -1;
-		}, 10)
+function doFuckery() {
+	doDrift();
+	doGravity();
+	setVolume();
+}
+
+function clamp(number, from, to) {
+	if (number < from) {
+		number = from;
+	} else if (number > to) {
+		number = to;
+	}
+	return number;
+}
+
+function doDrift() {
+	if (!FEATURE_DRIFT) return;
+	sliderLeft.value  = parseInt(sliderLeft.value)  + tickSize * leftDirection;
+	sliderRight.value = parseInt(sliderRight.value) + tickSize * rightDirection;
+}
+
+function doGravity() {
+	if (!FEATURE_GRAVITY) return;
+	if ((++gravityFlag) === gravityFlag) {
+		let _min = parseInt(volumeBar.min);
+		let _max = parseInt(volumeBar.max);
+		let _value = parseInt(volumeBar.value);
+		let center = (parseInt(volumeBar.max) + parseInt(volumeBar.min)) / 2;
+
+		let weight = Math.round(gravityTick * ((_value - center)) / (_max - _min));
+
+		sliderLeft.value  = parseInt(sliderLeft.value)  + weight;
+		sliderRight.value = parseInt(sliderRight.value) - weight;
+	} else {
+		gravityFlag = 0;
 	}
 }
+
